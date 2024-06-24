49e3a766016121d58de67100f2d8954c257d318e
PatrickR <96024158+PatrickRung@users.noreply.github.com>
2024-06-24 14:37:49 -0700

    New speed controller
    This one requires use of potentiometer

--- DroneCode/DroneCode.ino
+++ DroneCode/DroneCode.ino
@@ -1,12 +1,11 @@
 #include <Servo.h>
 
+
 Servo ESC;
+
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println("started!");
-  //sets up and arms the ESC using PWM frequencies meaning that we are using 1000 as
-  //min throttle and 2000 as max. The 9 represents the pin that we use as a signal.
-  //THIS MUST BE THE FIRST LINE IN SETUP AND MUST BE FOLLOWED BY 5000 DELAY. Why idk
+Serial.begin(9600);
 }
 
 
@@ -15,24 +14,35 @@ void loop() {
     int val = Serial.parseInt();
     if (val == 1) {
       Serial.println("arming sequence initiated");
-      ESC.attach(9, 1000, 2000);
+      ESC.attach(A1, 1000, 2000);
+      delay(500);
       ESC.writeMicroseconds(2000);
-      delay(1000);
+      delay(500);
       ESC.writeMicroseconds(1000);
       delay(5000);
+      ESC.writeMicroseconds(1000);
       Serial.println("done");
 
     } else if (val == 2) {
+      Serial.println("spin at 10% speed");
+      ESC.writeMicroseconds(1100);
+      delay(1000);
       Serial.println("spin at 20% speed");
       ESC.writeMicroseconds(1200);
+      delay(1000);
+      Serial.println("spin at 30% speed");
+      ESC.writeMicroseconds(1300);
       delay(5000);
+      Serial.println("spin at 40% speed");
+      ESC.writeMicroseconds(1400);
+      delay(5000);
+      ESC.writeMicroseconds(1000);
       Serial.println("done");
-      ESC.write(1000);
     } 
     else if(val == 3){
       Serial.println("detatching");
       ESC.detach();
     }
-
+    
   }
 }
---
+++ ManualSpeedControl/ManualSpeedControl.ino
@@ -0,0 +1,22 @@
+
+
+#include <Servo.h>
+
+Servo ESC;     // create servo object to control the ESC
+
+int potValue;  // value from the analog pin
+
+void setup() {
+  // Attach the ESC on pin 9
+  Serial.begin(9600);
+  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+}
+
+void loop() {
+  
+  potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
+  potValue = map(potValue, 0, 1023, 35, 1000);   // scale it to use it with the servo library (value between 0 and 180)
+  //the actual value for this esc is 35 = min puls and max is something idk (temp is 215)
+  Serial.println(potValue);
+  ESC.write(potValue);    // Send the signal to the ESC
+}
